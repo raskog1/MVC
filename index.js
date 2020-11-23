23e731e8043839c76d7db9f6d2944fd4d4bd0672
Ryan Skog <ryan.skog@outlook.com>
2020-11-22 16:23:41 -0800

    Created a simple pagination with page refresh on click

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.addComponent({
     images: [],
     abbrImages: [],
     perPageImage: 3,
+    pageNum: 1,
   },
 
   view(model) {
@@ -33,19 +34,35 @@ app.addComponent({
   },
 
   async controller(model) {
+    // Handling initial view of three images
+    const images = await API.getImages();
+    model.images = images;
+    populatePage();
+    app.updateView();
+
+    // Handling page number clicks
+    const buttonPush = document.querySelector(".pageLinks");
+    buttonPush.addEventListener("click", (e) => {
+      app.clearView();
+      model.pageNum = parseInt(e.target.innerHTML);
+      populatePage();
+    });
+
     // Handling a change in images per page
     const perPage = document.querySelector("#pages");
     perPage.addEventListener("change", (e) => {
-      model.perPageImage = e.target.value;
+      model.perPageImage = parseInt(e.target.value);
       model.abbrImages = images.slice(0, model.perPageImage);
       app.updateView();
     });
 
-    // Handling initial view of three images
-    const images = await API.getImages();
-    model.images = images;
-    model.abbrImages = images.slice(0, model.perPageImage);
-    app.updateView();
+    // Populate page based on current model
+    function populatePage() {
+      const { pageNum, perPageImage } = model;
+      const sliceIndex = (pageNum - 1) * perPageImage;
+      model.abbrImages = images.slice(sliceIndex, sliceIndex + perPageImage);
+      app.updateView();
+    }
   },
 });
 
